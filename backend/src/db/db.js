189d5060d6f3cbf93d2f9f31365f3dad140d706f
Naimish Omar <naimishomar@gmail.com>
2026-05-24 20:40:53 +0530

    frontend updated

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,10 +1,16 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config({quiet: true});
 
-export const connectDB = async(req,res)=>{
+export const connectDB = async () => {
     try {
-        mongoose.connect(process.env.MONGO_URI);
+        if (!process.env.MONGO_URI) {
+            console.error("MONGO_URI is not defined in the environment variables!");
+            return;
+        }
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("Connected to MongoDB");
     } catch (error) {
-        console.log("Error in mongoDB connection", error);   
+        console.error("Error in mongoDB connection:", error);   
     }
 }
